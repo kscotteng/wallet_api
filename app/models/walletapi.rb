31cfef0ef2ce046b8d75d182424008b3ebbc9e9f
kscotteng <kscott@gmail.com>
2016-03-02 17:33:23 -0500

    add balance class method, add associated test

--- app/models/walletapi.rb
+++ app/models/walletapi.rb
@@ -1,2 +1,12 @@
 class Walletapi < ActiveRecord::Base
+
+  def self.balance
+    b = []
+      self.all.each do |x|
+        b << x.amount.to_f
+      end
+      total = b.sum
+
+
+  end
 end
--- test/models/walletapi_test.rb
+++ test/models/walletapi_test.rb
@@ -4,4 +4,9 @@ class WalletapiTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "testing amount balance" do
+    assert_equal 25.00, Walletapi.balance
+
+  end
 end
